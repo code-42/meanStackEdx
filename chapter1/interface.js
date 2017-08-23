eda8881559ca2a9348e7a1ac25d477b6b4bc6456
Edward Dupre <edwd42@gmail.com>
2017-08-23 19:59:59 +0000

    initial commit

---
+++ chapter1/interface.js
@@ -0,0 +1,34 @@
+/*
+ *  Inserts "doc" into the collection "movies".
+ */
+exports.insert = function(db, doc, callback) {
+  // TODO: implement
+  db.collection('movies').insert(doc, function(error, result){
+    callback(error);
+  })
+  // callback(null);
+};
+
+/*
+ *  Finds all documents in the "movies" collection
+ *  whose "director" field equals the given director,
+ *  ordered by the movie's "title" field. See
+ *  http://mongodb.github.io/node-mongodb-native/2.0/api/Cursor.html#sort
+ */
+exports.byDirector = function(db, director, callback) {
+  // TODO: implement
+      // Do normal ascending sort
+    db.collection('movies').find({'director':director}).sort({'title':1}).toArray(function(error, docs){
+      if (error){
+        console.log("line 25. error == ", error);
+        // process.exit(1);
+        callback(error);
+      }
+      console.log("28. found docs: ");
+      docs.forEach(function(doc){
+        console.log(JSON.stringify(doc));
+      });
+      
+      callback(null, docs);
+    });
+};
